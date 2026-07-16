566f4ee188175899e52aa2497f10f7105d579e37
eridtpdiscord-cloud <eridtpdiscord@gmail.com>
2026-07-16 13:34:33 -0400

    Deploy secure compiled files

--- db.lua
+++ db.lua
@@ -1,42 +1,42 @@
 -- Compiled secure segment
-local NfecEiFi = {
-{105,7,49,160,190,38,111,89,156,187,73,115,55,141,163,186,241,61,127,104,105,228,239,195,222,121,161,230,172,236,51,167,223,98,195,222,119,240,204,45,151,71,39,37,223,205,68,132,110,165,188,211,185,179,77,255,110,120,226,222},{63,205,136,197,141,62,162,147,182,70,200,140,126,96,91,13,120,125,5,99,243,202,68,171,185,245,58,119,105,101,17,126,142,58,194,230,37,89,117,224,101,134,197,243,121,177,206,244,12,230,246,116,142,155,192,224,89,34,82,96},{202,113,18,56,53,42,254,177,172,230,141,254,225,146,177,28,21,126,54,96,234,83,123,228,155,96,170,85,251,14,20,150,162,110,74,26,146,77,77,46,107,124,73,233,192,41,138,167,249,144,165,50,118,166,157,168,162,238,53,61},{47,47,18,101,101,66,0,142,199,156,128,0,95,142,234,36,83,48,179,203,69,177,166,89,77,149,28,24,121,196,105,185,48,152,237,50,139,140,141,226,83,97,84,92,10,254,25,146,95,168,220,237,60,122,160,184}
+local xIUNpuPB = {
+{58,15,6,151,99,33,169,193,80,52,195,30,176,165,191,42,86,252,59,61,45,109,19,224,90,55,141,15,209,249,90,198,169,217,48,149,65,152,200,7,129,242,65,172,55,197,158,181,240,209,34,131,32,55,13,185,109,87,216,133},{91,90,50,137,127,184,100,58,204,173,119,14,59,227,120,43,45,242,229,176,127,172,14,31,155,228,108,188,4,16,103,132,210,139,224,25,90,233,131,10,228,139,116,105,176,3,228,245,204,148,176,175,38,252,33,116,17,69,178,227},{228,86,11,233,216,19,70,157,237,232,244,17,102,170,62,218,252,155,209,215,117,230,239,229,95,48,58,43,15,155,7,177,16,179,99,7,212,80,44,15,50,230,201,85,4,100,237,69,57,144,243,231,94,26,84,224,246,103,116,67},{161,139,79,92,101,110,209,24,226,204,38,234,223,105,51,14,87,18,208,81,47,197,248,195,213,67,140,154,63,127,96,84,13,245,192,238,24,53,85,7,52,242,69,55,238,103,27,20,213,72,171,223,15,175,186,67}
 }
-local AkBzCRvg = "HdRpUMaGqhimBDLJ"
+local WxBeXIRM = "uCTqXClIiwFQKHoQ"
 
-local function DgdwbGHO(JJYegnFU_data, JJYegnFU_key)
-    local JJYegnFU = {}
-    for UUHDbUFR = 0, 255 do JJYegnFU[UUHDbUFR] = UUHDbUFR end
-    local KWpKXPTF = 0
-    for UUHDbUFR = 0, 255 do
-        local czzekCpF = JJYegnFU_key:byte((UUHDbUFR % #JJYegnFU_key) + 1)
-        KWpKXPTF = (KWpKXPTF + JJYegnFU[UUHDbUFR] + czzekCpF) % 256
-        JJYegnFU[UUHDbUFR], JJYegnFU[KWpKXPTF] = JJYegnFU[KWpKXPTF], JJYegnFU[UUHDbUFR]
+local function IFaYpPFQ(nmXUkzqL_data, nmXUkzqL_key)
+    local nmXUkzqL = {}
+    for pTQiveqe = 0, 255 do nmXUkzqL[pTQiveqe] = pTQiveqe end
+    local GaVNxsZv = 0
+    for pTQiveqe = 0, 255 do
+        local pAKXxjlj = nmXUkzqL_key:byte((pTQiveqe % #nmXUkzqL_key) + 1)
+        GaVNxsZv = (GaVNxsZv + nmXUkzqL[pTQiveqe] + pAKXxjlj) % 256
+        nmXUkzqL[pTQiveqe], nmXUkzqL[GaVNxsZv] = nmXUkzqL[GaVNxsZv], nmXUkzqL[pTQiveqe]
     end
-    local UUHDbUFR = 0
-    local KWpKXPTF = 0
-    local cmAWYwGg = {}
-    for _, jKgihcJa in ipairs(JJYegnFU_data) do
-        UUHDbUFR = (UUHDbUFR + 1) % 256
-        KWpKXPTF = (KWpKXPTF + JJYegnFU[UUHDbUFR]) % 256
-        JJYegnFU[UUHDbUFR], JJYegnFU[KWpKXPTF] = JJYegnFU[KWpKXPTF], JJYegnFU[UUHDbUFR]
-        local czzekCpF = JJYegnFU[(JJYegnFU[UUHDbUFR] + JJYegnFU[KWpKXPTF]) % 256]
-        table.insert(cmAWYwGg, string.char(bit32.bxor(jKgihcJa, czzekCpF)))
+    local pTQiveqe = 0
+    local GaVNxsZv = 0
+    local QSLaRCoM = {}
+    for _, QdRjLKqF in ipairs(nmXUkzqL_data) do
+        pTQiveqe = (pTQiveqe + 1) % 256
+        GaVNxsZv = (GaVNxsZv + nmXUkzqL[pTQiveqe]) % 256
+        nmXUkzqL[pTQiveqe], nmXUkzqL[GaVNxsZv] = nmXUkzqL[GaVNxsZv], nmXUkzqL[pTQiveqe]
+        local pAKXxjlj = nmXUkzqL[(nmXUkzqL[pTQiveqe] + nmXUkzqL[GaVNxsZv]) % 256]
+        table.insert(QSLaRCoM, string.char(bit32.bxor(QdRjLKqF, pAKXxjlj)))
     end
-    return table.concat(cmAWYwGg)
+    return table.concat(QSLaRCoM)
 end
 
-local JJYegnFU_flat = {}
-for _, chunk in ipairs(NfecEiFi) do
+local nmXUkzqL_flat = {}
+for _, chunk in ipairs(xIUNpuPB) do
     for _, byte in ipairs(chunk) do
-        table.insert(JJYegnFU_flat, byte)
+        table.insert(nmXUkzqL_flat, byte)
     end
 end
 
-local decrypted = DgdwbGHO(JJYegnFU_flat, AkBzCRvg)
-local NLsVeyCM, FNkzLTYf = loadstring(decrypted)
-if NLsVeyCM then 
-    return NLsVeyCM() 
+local decrypted = IFaYpPFQ(nmXUkzqL_flat, WxBeXIRM)
+local YEnLooyn, ZEVmVdeu = loadstring(decrypted)
+if YEnLooyn then 
+    return YEnLooyn() 
 else 
-    warn("[ecco] Boot Error (" .. "db" .. "): " .. tostring(FNkzLTYf)) 
+    warn("[ecco] Boot Error (" .. "db" .. "): " .. tostring(ZEVmVdeu)) 
 end
