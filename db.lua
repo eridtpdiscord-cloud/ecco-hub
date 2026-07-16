977b2609267d050c0e3fd33b5ebf9e07936f8bd6
eridtpdiscord-cloud <eridtpdiscord@gmail.com>
2026-07-16 14:14:28 -0400

    Deploy secure compiled files

--- db.lua
+++ db.lua
@@ -1,42 +1,42 @@
 -- Compiled secure segment
-local vmeRlHID = {
-{245,102,136,140,21,163,105,30,237,81,115,139,173,233,91,240,202,101,2,132,5,194,58,161,0,154,45,101,211,134,10,167,205,73,132,223,220,215,34,136,210,51,233,1,50,224,119,34,97,168,79,196,160,125,95,209,164,169,1,70},{225,109,246,175,161,100,59,27,107,244,177,75,228,212,82,241,130,23,2,55,36,219,191,16,154,68,21,119,46,157,205,97,40,140,162,249,242,102,204,81,67,66,214,164,132,125,19,35,196,122,92,197,9,22,198,205,236,92,243,92},{162,179,125,175,243,201,251,212,110,21,224,64,166,247,83,188,247,244,136,134,91,50,155,53,122,226,244,20,73,85,149,161,33,180,29,194,231,181,252,203,78,123,58,246,135,28,101,1,19,152,149,173,113,168,39,181,152,155,35,7},{90,113,197,57,229,67,136,114,119,81,19,191,10,93,133,74,64,140,66,65,235,210,166,36,14,46,230,65,163,160,34,99,45,155,6,112,57,190,35,175,222,210,102,44,31,42,148,65,69,132,88,214,245,89,247,107,10,8,226,2},{88,29,203,156,186,103,246,104,234,24,119,10,193,209,79,19,124,203,18,155,1,145,49,212,57,135,165,99,201,155,105,156,132,80,55,25,133,162,149,52,181,163,4,160,111,37,187,123,69,174,28,218,136,179,108,194,132,250,19,131},{42,150,62,71,185,170,142,72,191,226,87,69,104,142,69,93,80,203,208,223,24,13,205,122,152,124,53,135,253,41,123,151,35,136,216,235,146,62,183,127,221,108,93,253,90,169,44,214,1,77,233,144,171,122,177,94,93,204,44,158},{188,93,41,253,252,108,74,72,36,1,26,124,81,20,89,212,18,99,213,125,10,211,108,22,91,15,177,82,166,136,86,60,205,248,105,255,236,16,122,44,133,72,214,69,144,234,233,247,238,216,40,178,55,212,187,236,135,232,105,249},{117,180,86,248,114,189,11,71,146,186,153,78,16,210,230,53,77,220,127,96,75,173,192}
+local ItMqOuNA = {
+{46,236,175,180,217,150,171,104,19,153,203,165,218,219,120,225,152,106,31,128,56,64,117,205,247,223,219,182,158,21,48,7,39,166,80,176,0,254,52,188,88,52,66,99,214,116,5,169,87,30,40,177,216,199,199,149,75,29,37,43},{238,214,164,168,64,250,230,2,202,103,251,52,148,71,209,216,82,207,97,232,60,147,136,102,235,57,0,30,23,93,69,65,174,31,108,168,180,65,245,111,54,213,153,180,123,172,73,13,202,32,171,71,70,228,52,140,77,228,10,86},{28,87,130,44,15,107,190,55,39,229,199,142,214,108,72,21,198,206,239,200,208,116,36,245,190,198,55,69,246,39,143,231,19,142,156,185,186,97,50,231,145,122,196,57,243,209,30,170,40,199,44,190,33,32,212,57,0,77,121,152},{160,80,232,194,28,88,43,141,85,248,74,42,1,75,178,186,35,186,93,148,2,194,46,188,198,248,12,159,115,177,65,236,231,122,38,15,237,213,150,137,5,99,175,161,49,220,7,89,29,211,11,3,155,159,115,6,48,63,163,213},{166,1,228,37,119,227,89,241,212,112,127,5,236,202,29,245,163,23,244,91,47,220,176,16,113,236,178,220,46,137,233,186,150,166,232,5,23,86,170,33,196,166,180,36,183,35,140,124,69,223,240,68,85,90,99,83,231,201,105,4},{44,22,123,198,237,227,58,78,59,52,209,194,118,15,114,58,59,216,238,92,18,58,128,184,93,23,173,95,220,73,70,176,181,178,185,215,226,94,228,129,231,48,92,191,141,227,16,163,247,34,45,141,234,121,237,111,246,93,27,246},{80,139,110,87,86,159,249,38,202,181,24,34,231,47,217,61,31,154,64,114,69,33,211,172,40,114,218,230,117,248,4,108,250,20,169,248,22,59,171,83,112,202,97,215,213,14,194,101,200,137,243,139,247,84,88,224,241,98,90,242},{212,210,126,11,86,207,83,247,139,100,57,225,182,219,3,111,105,156,245,253,251,204}
 }
-local DrfVLfom = "COITPORyjBoIcUFJ"
+local BrIxXBLa = "TtVPHBzonGxfdtDM"
 
-local function SSAqjKuQ(AnCadFVk_data, AnCadFVk_key)
-    local AnCadFVk = {}
-    for XVZCrxVQ = 0, 255 do AnCadFVk[XVZCrxVQ] = XVZCrxVQ end
-    local smmjxxVW = 0
-    for XVZCrxVQ = 0, 255 do
-        local pmYcVDeQ = AnCadFVk_key:byte((XVZCrxVQ % #AnCadFVk_key) + 1)
-        smmjxxVW = (smmjxxVW + AnCadFVk[XVZCrxVQ] + pmYcVDeQ) % 256
-        AnCadFVk[XVZCrxVQ], AnCadFVk[smmjxxVW] = AnCadFVk[smmjxxVW], AnCadFVk[XVZCrxVQ]
+local function gGJddYik(VMUaBOys_data, VMUaBOys_key)
+    local VMUaBOys = {}
+    for fprHtUbV = 0, 255 do VMUaBOys[fprHtUbV] = fprHtUbV end
+    local RrlHhCoZ = 0
+    for fprHtUbV = 0, 255 do
+        local xolyUiaY = VMUaBOys_key:byte((fprHtUbV % #VMUaBOys_key) + 1)
+        RrlHhCoZ = (RrlHhCoZ + VMUaBOys[fprHtUbV] + xolyUiaY) % 256
+        VMUaBOys[fprHtUbV], VMUaBOys[RrlHhCoZ] = VMUaBOys[RrlHhCoZ], VMUaBOys[fprHtUbV]
     end
-    local XVZCrxVQ = 0
-    local smmjxxVW = 0
-    local cGxnkieS = {}
-    for _, PExLiClZ in ipairs(AnCadFVk_data) do
-        XVZCrxVQ = (XVZCrxVQ + 1) % 256
-        smmjxxVW = (smmjxxVW + AnCadFVk[XVZCrxVQ]) % 256
-        AnCadFVk[XVZCrxVQ], AnCadFVk[smmjxxVW] = AnCadFVk[smmjxxVW], AnCadFVk[XVZCrxVQ]
-        local pmYcVDeQ = AnCadFVk[(AnCadFVk[XVZCrxVQ] + AnCadFVk[smmjxxVW]) % 256]
-        table.insert(cGxnkieS, string.char(bit32.bxor(PExLiClZ, pmYcVDeQ)))
+    local fprHtUbV = 0
+    local RrlHhCoZ = 0
+    local QbvcnEIL = {}
+    for _, RcTSGjhU in ipairs(VMUaBOys_data) do
+        fprHtUbV = (fprHtUbV + 1) % 256
+        RrlHhCoZ = (RrlHhCoZ + VMUaBOys[fprHtUbV]) % 256
+        VMUaBOys[fprHtUbV], VMUaBOys[RrlHhCoZ] = VMUaBOys[RrlHhCoZ], VMUaBOys[fprHtUbV]
+        local xolyUiaY = VMUaBOys[(VMUaBOys[fprHtUbV] + VMUaBOys[RrlHhCoZ]) % 256]
+        table.insert(QbvcnEIL, string.char(bit32.bxor(RcTSGjhU, xolyUiaY)))
     end
-    return table.concat(cGxnkieS)
+    return table.concat(QbvcnEIL)
 end
 
-local AnCadFVk_flat = {}
-for _, chunk in ipairs(vmeRlHID) do
+local VMUaBOys_flat = {}
+for _, chunk in ipairs(ItMqOuNA) do
     for _, byte in ipairs(chunk) do
-        table.insert(AnCadFVk_flat, byte)
+        table.insert(VMUaBOys_flat, byte)
     end
 end
 
-local decrypted = SSAqjKuQ(AnCadFVk_flat, DrfVLfom)
-local wvEsNOqH, aIRFABkJ = loadstring(decrypted)
-if wvEsNOqH then 
-    return wvEsNOqH() 
+local decrypted = gGJddYik(VMUaBOys_flat, BrIxXBLa)
+local uFXKkncc, QvnXCvLV = loadstring(decrypted)
+if uFXKkncc then 
+    return uFXKkncc() 
 else 
-    warn("[ecco] Boot Error (" .. "db" .. "): " .. tostring(aIRFABkJ)) 
+    warn("[ecco] Boot Error (" .. "db" .. "): " .. tostring(QvnXCvLV)) 
 end
