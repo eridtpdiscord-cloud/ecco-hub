41eada65de54a4372e1a79bf36f5f0ab1635575c
eridtpdiscord-cloud <eridtpdiscord@gmail.com>
2026-07-17 22:06:27 -0400

    Deploy secure compiled files

--- db.lua
+++ db.lua
@@ -1,42 +1,42 @@
 -- Compiled secure segment
-local KVcJkTwO = {
-{203,170,46,41,166,255,222,28,94,147,239,149,84,190,142,196,210,170,175,199,131,130,53,26,235,89,16,222,206,188,6,28,165,47,98,248,240,218,121,76,155,128,103,192,77,23,255,63,164,116,154,143,229,18,9,68,191,228,206,165},{172,240,25,8,45,37,251,246,186,249,110,57,222,27,208,144,15,223,222,240,91,189,23,112,64,7,185,1,89,45,45,64,81,57,97,179,138,255,31,4,171,78,55,34,31,214,231,240,151,137,1,215,32,7,82,227,148,92,198,163},{142,195,239,3,108,156,163,187,48,141,93,40,219,40,93,91,74,93,220,174,166,46,132,161,61,87,132,162,192,248,137,17,163,20,200,231,126,150,57,70,14,161,236,121,48,39,56,58,223,19,154,217,177,51,220,6,23,193,106,248},{2,242,84,59,54,151,119,62,112,92,4,148,134,213,165,152,23,224,78,34,156,30,224,38,205,92,220,115,30,174,38,175,142,155,3,148,247,147,235,103,93,112,255,61,58,43,188,133,185,79,8,87,170,71,171,215,132,122,231,34},{85,161,42,34,97,33,27,206,40,70,7,121,121,233,221,69,95,164,22,44,47,92,34,136,175,143,223,39,178,191,93,36,35,199,121,173,27,238,241,29,144,146,184,18,253,239,169,113,214,2,170,208,53,10,101,16,95,173,232,197},{49,190,244,76,142,129,215,164,205,18,236,93,58,45,74,46,157,56,4,235,5,85,32,195,204,86,95,126,238,175,57,13,163,101,83,64,100,117,163,137,175,247,116,102,134,17,103,82,240,230,233,198,196,116,251,93,146,113,183,48},{15,109,248,81,121,222,121,248,55,154,46,176,94,24,66,49,127,4,146,168,28,112,25,251,147,122,243,69,165,50,16,181,148,85,147,167,67,220,144,3,145,113,220,44,58,97,212,103,39,95,20,31,144,152,10,78,24,40,217,147},{166,251,29,203,103,170,219,190,224,173,54,132,171,180,215,99,113,112,168,166,253,159,167,120}
+local YJxpFUDC = {
+{135,73,42,69,228,200,156,62,224,45,215,45,143,105,186,190,14,196,233,145,99,49,139,198,251,227,66,211,62,57,1,99,12,5,191,26,183,53,229,41,68,255,85,234,31,138,210,54,117,177,154,129,218,5,248,39,189,137,207,168},{158,132,84,189,125,193,184,45,119,130,213,246,59,202,107,104,161,248,209,168,60,21,35,89,83,63,252,35,83,15,99,4,85,147,157,62,210,199,99,232,129,45,212,62,64,88,66,181,219,43,135,190,8,113,253,142,209,178,140,184},{76,125,189,95,196,139,206,164,169,101,40,58,46,170,119,53,159,215,212,217,14,18,251,207,241,66,14,60,125,255,23,54,0,18,68,30,166,73,20,37,25,18,164,27,53,15,151,28,27,214,247,179,208,72,64,160,14,31,208,102},{85,92,146,149,234,114,62,204,25,205,12,49,167,165,83,59,226,5,197,103,231,190,78,150,183,110,11,6,189,140,236,51,195,17,202,137,128,119,223,42,209,152,1,160,121,245,188,231,16,41,233,65,84,175,99,101,136,108,37,87},{181,45,7,34,27,146,117,226,232,173,164,80,4,4,253,229,168,127,34,187,143,140,99,104,174,188,219,101,30,215,56,165,122,149,167,98,80,114,168,164,190,24,102,93,32,77,209,73,155,150,189,197,26,110,42,236,215,117,235,16},{106,114,35,45,141,103,254,148,180,186,181,244,34,143,179,234,222,64,153,51,35,181,2,143,16,97,122,144,50,51,195,8,75,183,63,122,75,118,83,20,16,253,82,64,92,96,7,136,44,247,223,35,33,56,81,49,19,213,122,117},{216,211,141,222,115,243,104,202,133,202,69,30,41,168,44,204,195,148,177,61,112,135,214,226,93,216,230,154,215,69,36,207,217,71,254,12,201,5,114,148,46,31,74,152,56,76,163,227,100,250,12,158,61,105,213,31,155,20,78,240},{179,79,17,202,115,112,158,61,86,234,237,52,48,140,173,126,18,198,97,248,84,177}
 }
-local jdGahkwa = "DRyapNKMRrJEAOwG"
+local DcTxyYuX = "LWzlUURMfSMMJxwT"
 
-local function ZQUfZcAG(LNUGZpZR_data, LNUGZpZR_key)
-    local LNUGZpZR = {}
-    for bVMmmQgC = 0, 255 do LNUGZpZR[bVMmmQgC] = bVMmmQgC end
-    local CFwHjRtb = 0
-    for bVMmmQgC = 0, 255 do
-        local tpztHhjW = LNUGZpZR_key:byte((bVMmmQgC % #LNUGZpZR_key) + 1)
-        CFwHjRtb = (CFwHjRtb + LNUGZpZR[bVMmmQgC] + tpztHhjW) % 256
-        LNUGZpZR[bVMmmQgC], LNUGZpZR[CFwHjRtb] = LNUGZpZR[CFwHjRtb], LNUGZpZR[bVMmmQgC]
+local function VMdOZQrB(TvgNeEKT_data, TvgNeEKT_key)
+    local TvgNeEKT = {}
+    for MjYiBuQs = 0, 255 do TvgNeEKT[MjYiBuQs] = MjYiBuQs end
+    local nbyXEuqE = 0
+    for MjYiBuQs = 0, 255 do
+        local GNghvbqv = TvgNeEKT_key:byte((MjYiBuQs % #TvgNeEKT_key) + 1)
+        nbyXEuqE = (nbyXEuqE + TvgNeEKT[MjYiBuQs] + GNghvbqv) % 256
+        TvgNeEKT[MjYiBuQs], TvgNeEKT[nbyXEuqE] = TvgNeEKT[nbyXEuqE], TvgNeEKT[MjYiBuQs]
     end
-    local bVMmmQgC = 0
-    local CFwHjRtb = 0
-    local CVlkOfiR = {}
-    for _, DzmOsxQX in ipairs(LNUGZpZR_data) do
-        bVMmmQgC = (bVMmmQgC + 1) % 256
-        CFwHjRtb = (CFwHjRtb + LNUGZpZR[bVMmmQgC]) % 256
-        LNUGZpZR[bVMmmQgC], LNUGZpZR[CFwHjRtb] = LNUGZpZR[CFwHjRtb], LNUGZpZR[bVMmmQgC]
-        local tpztHhjW = LNUGZpZR[(LNUGZpZR[bVMmmQgC] + LNUGZpZR[CFwHjRtb]) % 256]
-        table.insert(CVlkOfiR, string.char(bit32.bxor(DzmOsxQX, tpztHhjW)))
+    local MjYiBuQs = 0
+    local nbyXEuqE = 0
+    local bYDmesUU = {}
+    for _, mFsYjnYo in ipairs(TvgNeEKT_data) do
+        MjYiBuQs = (MjYiBuQs + 1) % 256
+        nbyXEuqE = (nbyXEuqE + TvgNeEKT[MjYiBuQs]) % 256
+        TvgNeEKT[MjYiBuQs], TvgNeEKT[nbyXEuqE] = TvgNeEKT[nbyXEuqE], TvgNeEKT[MjYiBuQs]
+        local GNghvbqv = TvgNeEKT[(TvgNeEKT[MjYiBuQs] + TvgNeEKT[nbyXEuqE]) % 256]
+        table.insert(bYDmesUU, string.char(bit32.bxor(mFsYjnYo, GNghvbqv)))
     end
-    return table.concat(CVlkOfiR)
+    return table.concat(bYDmesUU)
 end
 
-local LNUGZpZR_flat = {}
-for _, chunk in ipairs(KVcJkTwO) do
+local TvgNeEKT_flat = {}
+for _, chunk in ipairs(YJxpFUDC) do
     for _, byte in ipairs(chunk) do
-        table.insert(LNUGZpZR_flat, byte)
+        table.insert(TvgNeEKT_flat, byte)
     end
 end
 
-local decrypted = ZQUfZcAG(LNUGZpZR_flat, jdGahkwa)
-local aIrBieYC, rEbKsciZ = loadstring(decrypted)
-if aIrBieYC then 
-    return aIrBieYC() 
+local decrypted = VMdOZQrB(TvgNeEKT_flat, DcTxyYuX)
+local BejasPmb, vnHHBOtm = loadstring(decrypted)
+if BejasPmb then 
+    return BejasPmb() 
 else 
-    warn("[ecco] Boot Error (" .. "db" .. "): " .. tostring(rEbKsciZ)) 
+    warn("[ecco] Boot Error (" .. "db" .. "): " .. tostring(vnHHBOtm)) 
 end
