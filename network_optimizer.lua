9b07b9006337870db0d115a04c1be1c5486c5a1a
eridtpdiscord-cloud <eridtpdiscord@gmail.com>
2026-07-19 11:25:03 -0400

    Update via Axiom Compiler

--- network_optimizer.lua
+++ network_optimizer.lua
@@ -1,51 +1,51 @@
 -- Compiled secure segment
-local IIWFRlfw = {
-{103,59,124,137,166,86,200,233,143,53,33,150,127,216,224,17,106,188,136,103,57,246,184,107,216,2,156,35,135,184,49,253,223,158,152,48,30,96,143,252,47,126,222,33,7,169,143,183,4,225,52,244,231,172,32,175,31,12,227,248},{126,224,204,5,96,105,73,163,151,215,82,31,99,14,199,104,254,11,248,107,233,1,240,46,36,191,202,104,218,95,81,183,205,239,194,22,1,62,115,197,56,129,103,248,252,226,156,14,109,154,216,86,185,129,240,248,226,60,236,66},{69,37,184,176,219,83,156,224,128,175,215,217,12,208,33,118,132,10,76,64,57,236,188,21,150,152,164,231,12}
+local tocEenpY = {
+{38,150,23,134,68,220,225,62,156,67,128,204,145,109,106,227,29,136,80,249,12,139,110,234,17,178,76,21,61,229,185,163,214,112,5,27,219,180,227,107,118,7,170,66,213,176,167,111,50,60,116,166,210,34,73,184,231,247,243,53},{113,37,67,82,112,10,46,78,94,73,88,207,151,251,253,97,226,217,16,144,20,209,128,65,165,141,109,235,146,214,219,6,132,39,213,198,79,219,86,212,74,230,91,123,110,26,52,16,237,146,179,49,92,107,191,34,191,197,164,144},{138,44,22,130,154,155,216,39,177,145,153,114,241,157,163,234,196,140,67,64,251,108,150,3,133,112,205,110,254}
 }
-local tEQBFyZo = "qtPiiSySpmLXOYVv"
+local DQnCTkpG = "SoPWKuevpmIeiNwK"
 
-local function tRKRRtQb(OnMMVyFI_data, OnMMVyFI_key)
-    local OnMMVyFI = {}
-    for eaZKxUKM = 0, 255 do OnMMVyFI[eaZKxUKM] = eaZKxUKM end
-    local SNlDAIcJ = 0
-    for eaZKxUKM = 0, 255 do
-        local IWbSLIbN = OnMMVyFI_key:byte((eaZKxUKM % #OnMMVyFI_key) + 1)
-        SNlDAIcJ = (SNlDAIcJ + OnMMVyFI[eaZKxUKM] + IWbSLIbN) % 256
-        OnMMVyFI[eaZKxUKM], OnMMVyFI[SNlDAIcJ] = OnMMVyFI[SNlDAIcJ], OnMMVyFI[eaZKxUKM]
+local function FbljdTBd(vSqVlyYQ_data, vSqVlyYQ_key)
+    local vSqVlyYQ = {}
+    for WDxEUouc = 0, 255 do vSqVlyYQ[WDxEUouc] = WDxEUouc end
+    local OaWrrouP = 0
+    for WDxEUouc = 0, 255 do
+        local zjmzCSVt = vSqVlyYQ_key:byte((WDxEUouc % #vSqVlyYQ_key) + 1)
+        OaWrrouP = (OaWrrouP + vSqVlyYQ[WDxEUouc] + zjmzCSVt) % 256
+        vSqVlyYQ[WDxEUouc], vSqVlyYQ[OaWrrouP] = vSqVlyYQ[OaWrrouP], vSqVlyYQ[WDxEUouc]
     end
-    local eaZKxUKM = 0
-    local SNlDAIcJ = 0
-    local MscqjrQz = {}
-    for _, dpnnCpHs in ipairs(OnMMVyFI_data) do
-        eaZKxUKM = (eaZKxUKM + 1) % 256
-        SNlDAIcJ = (SNlDAIcJ + OnMMVyFI[eaZKxUKM]) % 256
-        OnMMVyFI[eaZKxUKM], OnMMVyFI[SNlDAIcJ] = OnMMVyFI[SNlDAIcJ], OnMMVyFI[eaZKxUKM]
-        local IWbSLIbN = OnMMVyFI[(OnMMVyFI[eaZKxUKM] + OnMMVyFI[SNlDAIcJ]) % 256]
-        table.insert(MscqjrQz, string.char(bit32.bxor(dpnnCpHs, IWbSLIbN)))
+    local WDxEUouc = 0
+    local OaWrrouP = 0
+    local pFtpcBWZ = {}
+    for _, UCUSDeQd in ipairs(vSqVlyYQ_data) do
+        WDxEUouc = (WDxEUouc + 1) % 256
+        OaWrrouP = (OaWrrouP + vSqVlyYQ[WDxEUouc]) % 256
+        vSqVlyYQ[WDxEUouc], vSqVlyYQ[OaWrrouP] = vSqVlyYQ[OaWrrouP], vSqVlyYQ[WDxEUouc]
+        local zjmzCSVt = vSqVlyYQ[(vSqVlyYQ[WDxEUouc] + vSqVlyYQ[OaWrrouP]) % 256]
+        table.insert(pFtpcBWZ, string.char(bit32.bxor(UCUSDeQd, zjmzCSVt)))
     end
-    return table.concat(MscqjrQz)
+    return table.concat(pFtpcBWZ)
 end
 
-local OnMMVyFI_flat = {}
-for _, chunk in ipairs(IIWFRlfw) do
+local vSqVlyYQ_flat = {}
+for _, chunk in ipairs(tocEenpY) do
     for _, byte in ipairs(chunk) do
-        table.insert(OnMMVyFI_flat, byte)
+        table.insert(vSqVlyYQ_flat, byte)
     end
 end
 
-local decrypted = tRKRRtQb(OnMMVyFI_flat, tEQBFyZo)
-local wSRtMLFy, ZjCxQBym = loadstring(decrypted)
+local decrypted = FbljdTBd(vSqVlyYQ_flat, DQnCTkpG)
+local vxGcYeFM, eYQNUwnM = loadstring(decrypted)
 
 -- Memory Sanitation: Overwrite decrypted variables to block RAM scan extraction
-table.clear(OnMMVyFI_flat)
-OnMMVyFI_flat = nil
+table.clear(vSqVlyYQ_flat)
+vSqVlyYQ_flat = nil
 
-if wSRtMLFy then 
-    local result = wSRtMLFy() 
+if vxGcYeFM then 
+    local result = vxGcYeFM() 
     decrypted = nil
-    wSRtMLFy = nil
+    vxGcYeFM = nil
     return result
 else 
     decrypted = nil
-    warn("[ecco] Boot Error (" .. "network_optimizer" .. "): " .. tostring(ZjCxQBym)) 
+    warn("[ecco] Boot Error (" .. "network_optimizer" .. "): " .. tostring(eYQNUwnM)) 
 end
