8ca874a1ec7bd92d58b77b45c2aa06584d005116
eridtpdiscord-cloud <eridtpdiscord@gmail.com>
2026-07-21 20:44:16 -0400

    Deploy secure compiled files

--- db.lua
+++ db.lua
@@ -1,53 +1,53 @@
 -- Protected Segment (db)
-local iYdrQRZZ = 81
-local KPZRRpxV = {
-{4,126,104,14,160,105,43,190,4,210,165,203,183,160,120,59,71,169,56,252,143,5,235,93,238,164,127,42,68,128,40,27,92,212,73,71,242,234,7,234,139,94,1,34,81,239,22,153,128,21,82,90,30,92,228,116,169,2,254,132},{210,14,177,58,80,67,1,255,225,209,162,87,248,91,228,37,210,240,180,30,203,186,102,204,96,201,179,216,63,229,155,30,64,40,194,186,212,123,129,26,250,171,119,69,26,241,217,171,89,20,207,21,116,179,12,135,25,229,42,113},{109,109,65,126,158,147,102,74,139,89,130,54,89,215,149,41,243,218,10,42,231,60,178,230,249,164,85,77,182,47,178,140,114,117,151,71,178,233,168,152,29,53,169,0,98,93,55,228,57,16,214,101,142,17,181,17,65,18,76,129},{36,109,50,123,232,88,134,117,23,83,120,253,51,178,140,142,170,122,137,106,15,50,113,221,245,85,243,124,224,24,63,248,60,154,107,167,64,216,234,60,61,15,177,8,174,54,110,188,95,26,102,119,31,118,221,252,5,71,28,43},{28,188,127,133,222,16,98,225,95,169,187,224,245,212,45,175,88,14,204,209,173,71,27,148,106,210,122,132,128,30,61,71,45,59,208,116,238,104,251,127,203,179,13,40,42,45,26,202,61,120,162,172,121,93,116,114,159,239,93,210},{99,45,241,134,162,246,185,163,2,28,33,92,7,46,161,137,163,221,83,71,147,148,114,174,230,181,26,234,234,149,248,185,141,209,145,53,39,92,221,191,204,6,31,190,125,190,255,55,27,24,192,209,133,146,251,158,217,167,193,53},{32,100,151,65,66,221,243,168,255,77,177,64,134,108,135,122,96,57,192,211,149,244,219,234,79,104,146,221,215,37,98,103,122,100,202,72,199,19,145,148,206,229,45,200,215}
+local BMYbYqLv = 103
+local oshbzPjY = {
+{16,87,118,149,203,66,34,56,177,22,104,171,64,241,86,185,207,66,89,6,39,168,201,53,76,111,231,133,87,238,158,249,254,241,45,205,24,48,32,183,236,104,25,168,192,20,170,42,183,135,79,15,248,188,117,20,180,165,117,202},{144,245,182,31,26,169,208,142,140,24,63,85,250,194,206,75,217,57,66,253,162,57,110,58,119,26,242,61,79,156,242,207,163,48,149,80,203,254,194,114,147,145,243,91,151,30,230,38,20,155,41,186,214,215,15,234,255,69,126,127},{229,151,129,109,85,31,45,232,50,223,81,199,206,52,235,131,122,219,247,140,217,34,215,184,142,204,162,179,175,75,82,243,12,207,233,198,37,252,88,37,79,61,119,77,108,104,56,136,154,161,150,3,149,122,208,214,225,25,116,19},{250,134,170,145,157,229,89,58,232,113,142,186,108,82,221,184,43,127,100,225,25,187,206,213,102,221,123,210,216,79,81,150,232,243,245,34,54,48,251,202,160,86,75,142,210,59,104,49,232,166,45,225,62,180,76,238,139,169,13,124},{218,47,39,43,133,216,170,49,10,186,172,126,36,168,56,45,150,162,160,106,240,81,148,60,82,152,38,76,178,126,78,255,179,144,192,38,32,118,84,198,63,245,106,71,103,160,15,220,136,189,17,62,123,31,253,205,150,215,137,170},{155,114,181,82,202,188,66,230,15,212,223,116,226,22,233,202,155,59,195,225,122,71,216,149,46,54,204,218,215,172,165,84,226,88,1,219,96,131,167,15,167,17,92,83,244,230,255,45,49,159,233,94,193,175,4,130,64,172,54,251},{30,135,22,46,255,88,131,210,226,171,26,87,123,71,35,124,166,155,15,184,160,34,65,102,192,137,212,128,54,142,223,131,162,184,81,104,208,8,109,255,6,89}
 }
-local PtQLCxqU = "uXiTEWxBvWsYWHII"
+local REJdTFgS = "GoRVTQhQnmkKnWpc"
 
-local function SdjFmqlM(tlCofRiB_data, tlCofRiB_key)
-    local tlCofRiB = {}
-    for AOgIFurV = 0, 255 do tlCofRiB[AOgIFurV] = AOgIFurV end
-    local FuwyLSAt = 0
-    for AOgIFurV = 0, 255 do
-        local nlSLPYQH = tlCofRiB_key:byte((AOgIFurV % #tlCofRiB_key) + 1)
-        FuwyLSAt = (FuwyLSAt + tlCofRiB[AOgIFurV] + nlSLPYQH) % 256
-        tlCofRiB[AOgIFurV], tlCofRiB[FuwyLSAt] = tlCofRiB[FuwyLSAt], tlCofRiB[AOgIFurV]
+local function YDbyLUDi(fwuYOXdb_data, fwuYOXdb_key)
+    local fwuYOXdb = {}
+    for aXmqLkws = 0, 255 do fwuYOXdb[aXmqLkws] = aXmqLkws end
+    local GxlrVZoD = 0
+    for aXmqLkws = 0, 255 do
+        local SfTDeKmd = fwuYOXdb_key:byte((aXmqLkws % #fwuYOXdb_key) + 1)
+        GxlrVZoD = (GxlrVZoD + fwuYOXdb[aXmqLkws] + SfTDeKmd) % 256
+        fwuYOXdb[aXmqLkws], fwuYOXdb[GxlrVZoD] = fwuYOXdb[GxlrVZoD], fwuYOXdb[aXmqLkws]
     end
-    local AOgIFurV = 0
-    local FuwyLSAt = 0
-    local YeXKurwO = {}
-    for _, bQdgiXWN in ipairs(tlCofRiB_data) do
-        AOgIFurV = (AOgIFurV + 1) % 256
-        FuwyLSAt = (FuwyLSAt + tlCofRiB[AOgIFurV]) % 256
-        tlCofRiB[AOgIFurV], tlCofRiB[FuwyLSAt] = tlCofRiB[FuwyLSAt], tlCofRiB[AOgIFurV]
-        local nlSLPYQH = tlCofRiB[(tlCofRiB[AOgIFurV] + tlCofRiB[FuwyLSAt]) % 256]
-        local unmasked = bit32.bxor(bQdgiXWN, iYdrQRZZ)
-        table.insert(YeXKurwO, string.char(bit32.bxor(unmasked, nlSLPYQH)))
+    local aXmqLkws = 0
+    local GxlrVZoD = 0
+    local MkrTNVBQ = {}
+    for _, tqHUNmsl in ipairs(fwuYOXdb_data) do
+        aXmqLkws = (aXmqLkws + 1) % 256
+        GxlrVZoD = (GxlrVZoD + fwuYOXdb[aXmqLkws]) % 256
+        fwuYOXdb[aXmqLkws], fwuYOXdb[GxlrVZoD] = fwuYOXdb[GxlrVZoD], fwuYOXdb[aXmqLkws]
+        local SfTDeKmd = fwuYOXdb[(fwuYOXdb[aXmqLkws] + fwuYOXdb[GxlrVZoD]) % 256]
+        local unmasked = bit32.bxor(tqHUNmsl, BMYbYqLv)
+        table.insert(MkrTNVBQ, string.char(bit32.bxor(unmasked, SfTDeKmd)))
     end
-    return table.concat(YeXKurwO)
+    return table.concat(MkrTNVBQ)
 end
 
-local tlCofRiB_flat = {}
-for _, chunk in ipairs(KPZRRpxV) do
+local fwuYOXdb_flat = {}
+for _, chunk in ipairs(oshbzPjY) do
     for _, byte in ipairs(chunk) do
-        table.insert(tlCofRiB_flat, byte)
+        table.insert(fwuYOXdb_flat, byte)
     end
 end
 
-local decrypted = SdjFmqlM(tlCofRiB_flat, PtQLCxqU)
-local zpaakZFL, WQcMRuvl = loadstring(decrypted)
+local decrypted = YDbyLUDi(fwuYOXdb_flat, REJdTFgS)
+local lixctMBo, TIogWzJH = loadstring(decrypted)
 
 -- Immediate Memory Sanitation: Overwrite decrypted variables
-table.clear(tlCofRiB_flat)
-tlCofRiB_flat = nil
+table.clear(fwuYOXdb_flat)
+fwuYOXdb_flat = nil
 
-if zpaakZFL then 
-    local result = zpaakZFL() 
+if lixctMBo then 
+    local result = lixctMBo() 
     decrypted = nil
-    zpaakZFL = nil
+    lixctMBo = nil
     return result
 else 
     decrypted = nil
-    warn("[ecco] Boot Error (" .. "db" .. "): " .. tostring(WQcMRuvl)) 
+    warn("[ecco] Boot Error (" .. "db" .. "): " .. tostring(TIogWzJH)) 
 end
